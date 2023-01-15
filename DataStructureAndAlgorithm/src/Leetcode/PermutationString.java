3c8558d3b2b2b3dbe895c4bc3fb956274797d627
Sanatkumar Dhobi <sanat541997@gmail.com>
2023-01-15 11:32:37 -0500

    How to Create permutation of string in Java

---
+++ DataStructureAndAlgorithm/src/Leetcode/PermutationString.java
@@ -0,0 +1,33 @@
+package Leetcode;
+
+
+/**
+ * @author Sanatkumar Dhobi
+ *	
+ * This is really simple Java Code to perform string permutations using recursion.
+ */
+public class PermutationString {
+	public static void main(String[] args) {
+		String str = "ab";
+		//System.out.println(str.substring(0,1) + str.substring(1+1));
+		permutation(str);
+		
+	}
+	
+	public static void permutation(String str) {
+		permutation(str, "");
+	}
+
+	private static void permutation(String str, String prefix) {
+		System.out.println("String :" + str + "      "+ "Prefix :"+ prefix);
+		if (str.length() == 0) {
+			System.out.println(prefix);
+		}else {
+			for (int i = 0; i < str.length(); i++) {
+				String rem = str.substring(0,i) + str.substring(i+1);
+				permutation(rem, prefix + str.charAt(i));
+			}
+		}
+		
+	}
+}
